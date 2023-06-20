2f344cf10a4f814b1f2f92e37042d9954fbd01b7
DmytroSkoryk <114485103+DmytroSkoryk@users.noreply.github.com>
2023-06-20 23:42:13 +0000

    Deploying to gh-pages from @ DmytroSkoryk/goit-react-hw-08-phonebook@ea583177e449e86ff186f21ee605af1d5404ca03 🚀

---
+++ static/js/290.1a88cc66.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[290],{7290:function(e,t,o){o.r(t),o.d(t,{default:function(){return r}});var a="Home_homePage__gNdkv",c="Home_titleHomePage__z3LNS",n=o(184);function r(){return(0,n.jsx)("div",{className:a,children:(0,n.jsx)("h1",{className:c,children:"Welcome to phonebook!"})})}}}]);
+//# sourceMappingURL=290.1a88cc66.chunk.js.map
